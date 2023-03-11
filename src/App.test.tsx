6d580c2ba636b48bb80d35e4d1045950d01bb654
M Kusold <kusrox@gmail.com>
2023-03-10 16:01:37 -0800

    rename

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './app';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const element = screen.getByText(/React Template/i);
-  expect(element).toBeInTheDocument();
-});
