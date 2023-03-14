dee499034d44fa2a8a51f6f1c9cda2a52e7db791
M Kusold <kusrox@gmail.com>
2023-03-14 10:48:32 -0700

    converting logo from default sx to styled syntax
    - I just really can't get into MUI's new 'sx' syntax. I'm quickly developing an adversion to it, so let's nip it in the bud and go replace their default stylings manually with styled components

--- src/components/navbar/logo.tsx
+++ src/components/navbar/logo.tsx
@@ -1,53 +1,78 @@
 import * as React from 'react'
 import AdbIcon from '@mui/icons-material/Adb'
-import { Typography } from '@mui/material'
+import { styled, Typography } from '@mui/material'
+
+const Icon = styled(AdbIcon)(({ theme }) => ({
+  marginRight: theme.spacing(1),
+  [theme.breakpoints.up('md')]: {
+    display: 'flex'
+  },
+  [theme.breakpoints.down('md')]: {
+    display: 'none'
+  }
+}))
+
+const CompanyName = styled(Typography)(({ theme }) => ({
+  marginRight: theme.spacing(2),
+  fontFamily: 'monospace',
+  fontWeight: 700,
+  letterSpacing: '.3rem',
+  color: 'inherit',
+  textDecoration: 'none',
+  [theme.breakpoints.up('md')]: {
+    display: 'flex'
+  },
+  [theme.breakpoints.down('md')]: {
+    display: 'none'
+  }
+})) as typeof Typography
 
 export const Logo = () => {
   return (
   <>
-    <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-    <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none'
-            }}
-        >
+    <Icon/>
+    <CompanyName
+      variant="h6"
+      noWrap
+      component="a"
+      href="/">
             LOGO
-        </Typography>
-    </>)
+    </CompanyName>
+  </>
+  )
 }
+
+const MobileIcon = styled(AdbIcon)(({ theme }) => ({
+  marginRight: theme.spacing(1),
+  [theme.breakpoints.up('md')]: {
+    display: 'none'
+  },
+  [theme.breakpoints.down('md')]: {
+    display: 'flex'
+  }
+}))
+
+const MobileCompanyName = styled(CompanyName)(({ theme }) => ({
+  flexGrow: 1,
+  [theme.breakpoints.up('md')]: {
+    display: 'none'
+  },
+  [theme.breakpoints.down('md')]: {
+    display: 'flex'
+  }
+})) as typeof Typography
 
 export const MobileLogo = () => {
   return (
         <>
-         <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-          <Typography
+         <MobileIcon />
+         <MobileCompanyName
             variant="h5"
             noWrap
             component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none'
-            }}
-          >
+            href="/">
             LOGO
-          </Typography>
+            </MobileCompanyName>
         </>
   )
 }
